4483eaffb4d1912fbe0d92d5e425b40cad5667d5
Mustafa Ozcan <mustafa_ozcan_@hotmail.com>
2025-05-04 17:41:11 +0300

    Add project files.

---
+++ OnlineEdu.Entity/Entities/About.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.Entity.Entities
+{
+    public class About
+    {
+        public int AboutId { get; set; }
+        public string Description { get; set; }
+        public int ImageUrl1 { get; set; }
+        public int ImageUrl2 { get; set; }
+        public int Item1 { get; set; }
+        public int Item2 { get; set; }
+        public int Item3 { get; set; }
+        public int Item4 { get; set; }
+    }
+}
